e144fa44e0c298676c63a07c1f195b4c6c845ed6
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-19 04:02:06 +0200

    refactor: modify the response of login to align with best practices

--- src/main/java/dev/kiki/walletapi/user/dto/LoginResponse.java
+++ src/main/java/dev/kiki/walletapi/user/dto/LoginResponse.java
@@ -1,8 +1,31 @@
 package dev.kiki.walletapi.user.dto;
 
+import dev.kiki.walletapi.user.User;
+
+import java.util.UUID;
+
 public record LoginResponse(
         String message,
         String accessToken,
-        Long expiresIn
+        Long expiresIn,
+        SafeUser user
 ) {
+
+    public record SafeUser(
+            UUID id,
+            String firstName,
+            String lastName,
+            String email
+    ) {
+        // Static method to convert User to SafeUser
+        public static SafeUser fromUser(User user) {
+            return new SafeUser(
+                    user.getId(),
+                    user.getFirstName(),
+                    user.getLastName(),
+                    user.getEmail()
+            );
+        }
+    }
+
 }

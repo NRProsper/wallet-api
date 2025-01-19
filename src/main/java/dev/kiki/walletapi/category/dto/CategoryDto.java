88af836761d11d44a645ff8055002294a15150bb
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-19 02:51:41 +0200

    ft: add way to add emojis on categories

--- src/main/java/dev/kiki/walletapi/category/dto/CategoryDto.java
+++ src/main/java/dev/kiki/walletapi/category/dto/CategoryDto.java
@@ -1,12 +1,19 @@
 package dev.kiki.walletapi.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CategoryDto(
 
         @NotBlank(message = "Category name is required")
         @Size(min = 3, message = "Category name should be at least 3 characters")
-        String name
+        String name,
+
+        @Pattern(
+                regexp = "[\uD83C-\uDBFF\uDC00-\uDFFF]+",
+                message = "Invalid emoji. Please provide a valid emoji character."
+        )
+        String emoji
 ) {
 }
